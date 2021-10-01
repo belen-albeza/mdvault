ef2656129b9f4d216461028ab26f5e13ef7588a2
Belén Albeza <belen.albeza@gmail.com>
2021-10-01 18:57:17 +0200

    Add example for listing all files within a vault

---
+++ examples/list_vault_files.rs
@@ -0,0 +1,15 @@
+use mdvault::Vault;
+use std::path::PathBuf;
+
+fn main() {
+    let vault = Vault::new(&PathBuf::from("tests/fixtures/basic"));
+    println!("Vault at {:?}", vault.base_path);
+    println!("Notes ({}):", vault.notes.len());
+    for note in vault.notes {
+        println!("\t{:#?}", note.file.rel_path().unwrap())
+    }
+    println!("Assets ({}):", vault.assets.len());
+    for asset in vault.assets {
+        println!("\t{:?}", asset.file.rel_path().unwrap())
+    }
+}
